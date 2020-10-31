884a63d3daa028830c092b8731f8c75e02a6fcdd
Nelio Alves <acenelio@gmail.com>
2020-10-31 19:23:24 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/DslearnbdsApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslearnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnbdsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dslearnbds/DslearnbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslearnbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnbdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
