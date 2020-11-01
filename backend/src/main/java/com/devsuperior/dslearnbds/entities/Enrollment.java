7ad8579af82b98b5772d528a549c73947f8b2e62
Nelio Alves <acenelio@gmail.com>
2020-10-31 21:32:35 -0300

    Enrollment, EnrollmentPK

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enrollment.java
@@ -0,0 +1,91 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
+
+@Entity
+@Table(name = "tb_enrollment")
+public class Enrollment {
+
+	@EmbeddedId
+	private EnrollmentPK id = new EnrollmentPK();
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant enrollMoment;
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant refundMoment;
+	private boolean available;
+	private boolean onlyUpdate;
+	
+	public Enrollment() {
+	}
+
+	public Enrollment(User user, Offer offer, Instant enrollMoment, Instant refundMoment, boolean available,
+			boolean onlyUpdate) {
+		super();
+		id.setUser(user);
+		id.setOffer(offer);
+		this.enrollMoment = enrollMoment;
+		this.refundMoment = refundMoment;
+		this.available = available;
+		this.onlyUpdate = onlyUpdate;
+	}
+
+	public User getStudent() {
+		return id.getUser();
+	}
+	
+	public void setStudent(User user) {
+		id.setUser(user);
+	}
+
+	public Offer getOffer() {
+		return id.getOffer();
+	}
+	
+	public void setOffer(Offer offer) {
+		id.setOffer(offer);
+	}
+	
+	public Instant getEnrollMoment() {
+		return enrollMoment;
+	}
+
+	public void setEnrollMoment(Instant enrollMoment) {
+		this.enrollMoment = enrollMoment;
+	}
+
+	public Instant getRefundMoment() {
+		return refundMoment;
+	}
+
+	public void setRefundMoment(Instant refundMoment) {
+		this.refundMoment = refundMoment;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public boolean isOnlyUpdate() {
+		return onlyUpdate;
+	}
+
+	public void setOnlyUpdate(boolean onlyUpdate) {
+		this.onlyUpdate = onlyUpdate;
+	}
+
+	
+	
+}
---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/pk/EnrollmentPK.java
@@ -0,0 +1,79 @@
+package com.devsuperior.dslearnbds.entities.pk;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.devsuperior.dslearnbds.entities.Offer;
+import com.devsuperior.dslearnbds.entities.User;
+
+@Embeddable
+public class EnrollmentPK implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne
+	@JoinColumn(name = "offer_id")
+	private Offer offer;
+	
+	public EnrollmentPK() {
+	}
+
+	public EnrollmentPK(User user, Offer offer) {
+		super();
+		this.user = user;
+		this.offer = offer;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Offer getOffer() {
+		return offer;
+	}
+
+	public void setOffer(Offer offer) {
+		this.offer = offer;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((offer == null) ? 0 : offer.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EnrollmentPK other = (EnrollmentPK) obj;
+		if (offer == null) {
+			if (other.offer != null)
+				return false;
+		} else if (!offer.equals(other.offer))
+			return false;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		return true;
+	}
+}
